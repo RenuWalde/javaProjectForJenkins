47465fb1f0e8b80a4ddd9a6ea7f470f4a2c6c094
RenuWalde <renuwalde@gmail.com>
2026-05-02 12:20:38 +0530

    Fix typo in display method output

--- src/main/java/myJenkinsPackage/Program1.java
+++ src/main/java/myJenkinsPackage/Program1.java
@@ -3,7 +3,7 @@ package myJenkinsPackage;
 public class Program1 {
 
 	public void display() {
-		System.out.println("Hello word");
+		System.out.println("Hello word")
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
