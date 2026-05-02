140ac3e192ef36193327e1b0598900d7d5994439
RenuWalde <renuwalde@gmail.com>
2026-05-02 11:01:47 +0530

    Initial commit

---
+++ src/main/java/myJenkinsPackage/Program1.java
@@ -0,0 +1,14 @@
+package myJenkinsPackage;
+
+public class Program1 {
+
+	public void display() {
+		System.out.println("Hello word");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Program1 p=new Program1();
+		p.display();
+	}
+
+}
